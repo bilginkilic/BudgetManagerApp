c514d775b28901409354b8e17ca77cfe5fb1d31f
Bilgin Kılıç <bilgin.kilic@hotmail.com>
2018-09-09 23:11:52 +0300

    ok

---
+++ BudgetManagerApp/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetManagerApp.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext()
+        {
+
+        }
+
+        public DataContext(  DbContextOptions<DataContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ BudgetManagerApp/Data/HistoryData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetManagerApp.Data
+{
+    public class HistoryData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Amount { get; set; }
+        /// <summary>
+        /// 1: Income; 2: Expense
+        /// </summary>
+        public int OperationType { get; set; }
+        public string Explanation { get; set; }
+        public DateTime StartTime { get; set; }
+        /// <summary>
+        /// 1:Monthly, 2: Weekly , 3: Daily
+        /// </summary>
+        public int Occurance { get; set; }
+    }
+}
